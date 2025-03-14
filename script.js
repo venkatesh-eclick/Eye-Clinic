5652b22cf55df21600838210ee818cfbc5d3114c
venkatesh-eclick <venkatesh@a-eye.ai>
2025-03-14 15:16:15 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("appointment-form").addEventListener("submit", function (event) {
-    event.preventDefault();
-    document.getElementById("form-message").innerText = "Your appointment request has been received!";
-    this.reset();
-});
